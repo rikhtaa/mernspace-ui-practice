41cb158bfef00a741dc85b4e76a7d9c3f424d1d9
Rekhta Menahil <rekhta674@gmail.com>
2025-08-08 20:25:24 +0500

    add login endpoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import {RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import { ConfigProvider } from 'antd'
+
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
+    <QueryClientProvider client={queryClient}>
    <ConfigProvider theme={{
       token: {
         colorPrimary: '#F65F42',
@@ -13,4 +18,5 @@ createRoot(document.getElementById('root')!).render(
       <RouterProvider  router={router}/> 
 
     </ConfigProvider>
+    </QueryClientProvider>
 )
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,22 @@
-import { Button, Card, Checkbox, Flex, Input, Layout, Space } from "antd"
+import { Alert, Button, Card, Checkbox, Flex, Input, Layout, message, Space } from "antd"
 import { LockFilled, LockOutlined, UserOutlined } from "@ant-design/icons"
 import { Form } from "antd";
+import { useMutation } from "@tanstack/react-query";
+import { login, type Credentials } from "../../http/api";
 
+
+const loginUser = async (credentials: Credentials)=>{
+   const {data} = await login(credentials)
+   return data
+}
 const LoginPage = () => {
+  const {mutate, isError, error} = useMutation({
+    mutationKey: ['Login'],
+    mutationFn: loginUser,
+    onSuccess: async()=>{
+      console.log('Login successfully')
+    }
+  })
   return <>
   <Layout style={{height: '100vh', display: "grid", placeItems: "center"}}>
    <Space direction="vertical" align="center" size="large">
@@ -16,7 +30,23 @@ const LoginPage = () => {
    }
    style={{ width: 300}}
    >
-    <Form initialValues={{remember: true}} >
+    <Form 
+    initialValues={{remember: true}} 
+    onFinish={(values)=>{
+      mutate({email: values.Username, password: values.Password})
+                  console.log(values)
+
+
+    }}
+    >
+      {
+        isError && (
+          <Alert 
+          type="error"
+          message={error?.message}
+          />
+        )
+      }
       <Form.Item
       name="Username"
       rules={[{ required: true, message: 'Please input your Username!'}, {type: 'email', message: "Email is not valid"}]}  
