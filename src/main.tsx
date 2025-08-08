91b9a3bb036f3c94bfa92df67d00a6157d61d8eb
Rekhta Menahil <rekhta674@gmail.com>
2025-08-08 16:03:30 +0500

    add login form

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import { ConfigProvider } from 'antd'
 createRoot(document.getElementById('root')!).render(
-  <RouterProvider  router={router}/> 
+   <ConfigProvider theme={{
+      token: {
+        colorPrimary: '#F65F42',
+        colorLink: '#F65F42',
+      }
+    }}>
+      <RouterProvider  router={router}/> 
+
+    </ConfigProvider>
 )
