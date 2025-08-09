d8403261a5699b684ca4715f58caac1f1670f479
Rekhta Menahil <rekhta674@gmail.com>
2025-08-09 12:58:45 +0500

    add store

---
+++ store.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+export interface User {
+    email: string
+}
+
+
+interface AuthState {
+    user: null | User
+    setUser: (user: User)=> void
+}
+
+export const useAuthStore =create<AuthState>()(
+    devtools((set)=>({
+    user: null,
+    setUser: (user)=> set({user}),
+    logout: ()=> set({user: null})
+    }))
+)
